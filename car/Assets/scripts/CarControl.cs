c2bc47920a54849170d3cd597e1dc5443aef46c6
TunTun <tuntuncs36@gmail.com>
2020-12-23 00:33:32 +0630

    finish game

---
+++ car/Assets/scripts/CarControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody _rigidbody;
+    float verticalMovement = 0;
+    float horizontalMovement = 0;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalMovement = Input.GetAxis("Vertical");
+        if (verticalMovement != 0)
+        {
+            horizontalMovement = Input.GetAxis("Horizontal");
+
+        }
+        else
+        {
+
+            horizontalMovement = 0;
+
+        }
+
+
+    }
+}
---
+++ car/Assets/scripts/CarMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    private CharacterController _controller;
+    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 30f;
+    private float _gravity = -9.81f;
+    float verticalMovement = 0;
+    float horizontalMovement = 0;
+
+
+
+
+    void Start()
+    {
+        _controller = GetComponent <CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        verticalMovement = Input.GetAxis("Vertical");
+        if(verticalMovement != 0)
+        {
+            horizontalMovement = Input.GetAxis("Horizontal");
+            
+        }
+        else {
+
+            horizontalMovement = 0;
+            
+        }
+
+        Vector3 localDirection = new Vector3(verticalMovement, 0, 0); //Direction in local. There, x axis is vertical.
+        Vector3 localVelocity = localDirection * _speed;
+
+
+        transform.Rotate(0, horizontalMovement, 0); //rotate in the local y axix
+
+        Vector3 worldVelocity = transform.TransformDirection(localVelocity); //change local velocity to world velocity;
+        worldVelocity.y = _gravity;
+        _controller.Move(worldVelocity * Time.deltaTime);
+    }
+}
